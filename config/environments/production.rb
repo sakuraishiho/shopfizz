e913f5491104286bb9a1cd9fbbaaebe613c9ac6e
sakura <ichigomilk35412@gmail.com>
2024-12-14 15:17:48 +0900

    メール送信機能の設定追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,14 @@ Rails.application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  config.action_mailer.smtp_settings = {
+
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.delivery_method = :smtp
+  host = "#{ENV['shopfizz']}.herokuapp.com"
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
@@ -57,10 +64,6 @@ Rails.application.configure do
     authentication: 'plain',
     enable_starttls_auto: true
   }
-  
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
