28f8cce7f75a2d037e7ea22c47e45e2d47d6b600
sakura <ichigomilk35412@gmail.com>
2024-12-15 13:37:03 +0900

    webhooksの設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.stripe = {
     publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
     secret_key: ENV['STRIPE_SECRET_KEY'],
-    endpoint_secret: ENV['STRIPE_ENDPOINT_SECRET']
+    endpoint_secret: ENV['STRIPE_WEBHOOK_SECRET']
   }
   
   Stripe.api_key = config.stripe[:secret_key]
